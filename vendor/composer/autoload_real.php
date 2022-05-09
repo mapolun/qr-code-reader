da57e6cdb7213ec499315564b80cea1aa9698f83
wtt <1364576479@qq.com>
2022-05-09 19:43:17 +0800

    feat

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInit1123e376391f17eb021280259030bcdc
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInit1123e376391f17eb021280259030bcdc', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit1123e376391f17eb021280259030bcdc', 'loadClassLoader'));
--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
+        'reference' => '98b5485d0130e262d58679a582b1e262e83c89b9',
         'name' => 'administrator/qr-code-reader',
         'dev' => true,
     ),
     'versions' => array(
         'administrator/qr-code-reader' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => '98b5485d0130e262d58679a582b1e262e83c89b9',
             'dev_requirement' => false,
         ),
     ),
