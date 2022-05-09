319c4debced403b7c8ea1e2ee830d095168fb8f3
wtt <1364576479@qq.com>
2022-05-09 19:50:30 +0800

    feat

--- test.php
+++ test.php
@@ -8,6 +8,6 @@ require __DIR__ . '/vendor/autoload.php';
 
 use QrCodeReader\Lib\QrReader;
 
-$qrcode = new QrReader(__DIR__ . '/qr.png');  //图片路径
+$qrcode = new QrReader(__DIR__ . '/src/qr.png');  //图片路径
 $text = $qrcode->decode(); //返回识别后的文本
 echo $text;
